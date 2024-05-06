5746244f57bcf272c51447a8be14645cd35909ea
Giancarlo Joel Alvarez Gallardo <161910868+GiancarloDev21@users.noreply.github.com>
2024-05-05 20:03:49 -0500

    Cambios de router a fabrica

--- src/router/index.js
+++ src/router/index.js
@@ -4,28 +4,13 @@
  * Automatic routes for `./src/pages/*.vue`
  */
 
-import { createRouter, createWebHistory } from 'vue-router/auto';
-import { setupLayouts } from 'virtual:generated-layouts';
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: setupLayouts,
-});
+})
 
-// Agregar lógica para redirigir desde la ruta principal
-router.beforeEach((to, from, next) => {
-  if (to.path === '/') {
-    const token = localStorage.getItem('token');
-    if (token) {
-      // El usuario está autenticado, redirige al dashboard
-      next('/dashboard');
-    } else {
-      // El usuario no está autenticado, redirige al login
-      next('/login');
-    }
-  } else {
-    next(); // Continuar navegando a la ruta solicitada
-  }
-});
-
-export default router;
+export default router
