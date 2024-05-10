8e4899026d3b619478dabbeab398aef35c28dd8d
Christian <202010600@urp.edu.pe>
2024-05-09 19:33:58 -0500

    registrar merma

--- src/services/mermaService.js
+++ src/services/mermaService.js
@@ -7,4 +7,17 @@ async function obtenerMermas(){
     return data;
 }
 
-export {obtenerMermas};
+async function registrarMerma(datosMerma) {
+    const request_url = url + 'post';
+    const response = await fetch(request_url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datosMerma)
+    });
+    const data = await response.json();
+    return data
+}
+
+export {obtenerMermas, registrarMerma};
