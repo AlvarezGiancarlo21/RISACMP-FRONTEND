8b28eb6439cecea12b2a9837a7e28711f32491be
Giancarlo Joel Alvarez Gallardo <161910868+GiancarloDev21@users.noreply.github.com>
2024-05-16 04:04:27 -0500

    Mejora del gestionar Usuario

--- src/pages/dashboard/gestionar-usuarios.vue
+++ src/pages/dashboard/gestionar-usuarios.vue
@@ -271,9 +271,6 @@
             <v-col cols="12" sm="6">
               <v-select v-model="usuarioActual.sexo" :items="['Masculino','Femenino']" label="Sexo" required></v-select>
             </v-col>
-            <v-col cols="12" md="4" sm="6">
-              <v-text-field v-model="usuarioActual.password" label="Contraseña" type="password" required></v-text-field>
-            </v-col>
             <v-col cols="12" sm="6">
               <v-select
                 v-model="usuarioActual.tipoDocumento"
@@ -344,7 +341,6 @@ const nuevoUsuario = ref({
   nombres: '',
   apellidos: '',
   sexo: '',
-  password: '',
   tipoDocumento: '',
   numeroDocumento: '',
   telefono: '',
