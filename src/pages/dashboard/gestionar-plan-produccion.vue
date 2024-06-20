fd4d622c990d52fbd0b16e7faf989caf21301aca
jersonll <165190170+jersonll@users.noreply.github.com>
2024-06-20 11:33:58 -0500

    Plan Producion Gantt

--- src/pages/dashboard/gestionar-plan-produccion.vue
+++ src/pages/dashboard/gestionar-plan-produccion.vue
@@ -1,165 +1,336 @@
-<template> 
-    <v-row class="d-flex flex-column">
-        <v-col>
-            <br>
-            <h1>Calendario</h1>
-            <v-dialog v-model="dialog" max-width="900px" max-height="900px">
-                <template v-slot:activator="{ props: activatorProps }">
-                    <v-btn color="success" class="text-none font-weight-regular" prepend-icon="mdi-plus" 
-                    @click="dialog=true" v-bind="activatorProps">
-                        Registrar
-                    </v-btn>
-                </template>
-                <v-card>
-                    <v-card-text>
-                        <v-row>
-                            <v-col cols="12">
-                            <p class="text-left font-weight-bold">Orden de Trabajo: </p>
-                            <v-combobox v-model="nuevoEvento.title" cleareable label="Seleccionar Orden de Trabajo" :items="ordenesTrabajo">
-                            </v-combobox>
-                            <v-text-field label="Fecha y hora Inicio" v-model="nuevoEvento.start"></v-text-field>
-                            <v-text-field label="Fecha y hora Final" v-model="nuevoEvento.end"></v-text-field>
-                            </v-col>
-                            <v-col cols="12">
-                            <p class="text-left font-weight-bold">Color: </p>
-                            <v-combobox v-model="nuevoEvento.color" label="Seleccionar color"
-                                :items="['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1']">
-                            </v-combobox>
-                            </v-col>
-                        </v-row>    
-                        <v-row>   
-                            <v-col cols="8">
-                            </v-col>
-                            <v-col cols="2">
-                                <v-btn width="300" color="blue" class="text-none font-weight-regular" prepend-icon="mdi-check"
-                                    text="GUARDAR" v-bind="activatorProps" @click="agregarEvento">
-                                </v-btn>
-                            </v-col>
-                            <v-col cols="2">
-                                <v-btn width="300" color="red" class="text-none font-weight-regular" prepend-icon="mdi-close"
-                                    text="CANCELAR" v-bind="activatorProps" @click="limpiarFormulario">
-                                </v-btn>
-                            </v-col>
-                        </v-row> 
-                        
-                        
-                    </v-card-text>
-                </v-card>
-            </v-dialog>
-        </v-col>
-        <v-col>
-            <v-calendar
-            ref="calendar"
-            v-model="value"
-            :events="events"
-            :view-mode="type"
-            :weekdays="weekday"
-            ></v-calendar>
-        </v-col>
-    </v-row>
+<template>
+  <v-container>
+    <div v-if="dateV" class="d-md-flex justify-space-between">
+      <h2>{{ `${months[dateV.getMonth(date)]}, ${dateV.getYear(date)}` }}</h2>
+      <v-dialog v-model="dialog" max-width="900">
+          <template v-slot:activator="{ props: activatorProps }">
+            <v-btn color="success" class="text-none font-weight-regular" prepend-icon="mdi-plus" v-bind="activatorProps"
+              @click="() => {
+                dialog = true;
+                modoModal = 'Agregar';
+              }">
+              AGREGAR
+            </v-btn>
+          </template>
+          <v-card
+            :title="modoModal === 'Agregar' ? 'Agregar Produccion' : modoModal === 'Editar' ? 'Editar Produccion' : 'X'"
+            class="text-center">
+            <v-card-text>
+              <v-row dense v-if="modoModal === 'Agregar'">
+                <v-col cols="12">
+                  <p class="text-left font-weight-bold">Orden de Trabajo: </p>
+                  <v-combobox v-model="nuevoTrabajo.ordenTrabajo" cleareable :items="ordenesT">
+                  </v-combobox>
+                </v-col>
+                <v-col cols="6">
+                  <p class="text-left font-weight-bold">Dia Inicio: </p>
+                  <v-text-field v-model="nuevoTrabajo.fechaInicio" type="date"></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <p class="text-left font-weight-bold">Dia Final: </p>
+                  <v-text-field v-model="nuevoTrabajo.fechaFin" type="date"></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <p class="text-left font-weight-bold">Estado: </p>
+                  <v-combobox v-model="nuevoTrabajo.estado" cleareable :items="['Planificado','En progreso','Finalizado']">
+                  </v-combobox>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-btn width="300" color="blue" class="text-none font-weight-regular" prepend-icon="mdi-check"
+                    text="GUARDAR" @click="agregarOrdenTrabajo()">
+                  </v-btn>
+                </v-col>
+                <v-col cols="2">
+                  <v-btn width="300" color="red" class="text-none font-weight-regular" prepend-icon="mdi-close"
+                    text="CANCELAR" @click="limpiarFormulario()">
+                  </v-btn>
+                </v-col>
+              </v-row>
+
+              <v-row dense v-if="modoModal === 'Editar'">
+                <v-col cols="12">
+                  <p class="text-left font-weight-bold">Orden de Trabajo: </p>
+                  <v-combobox v-model="trabajoSeleccionado.ordenTrabajo" cleareable :items="ordenesT" disabled>
+                  </v-combobox>
+                </v-col>
+                <v-col cols="6">
+                  <p class="text-left font-weight-bold">Dia Inicio: </p>
+                  <v-text-field v-model="trabajoSeleccionado.fechaInicio" type="date"></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <p class="text-left font-weight-bold">Dia Final: </p>
+                  <v-text-field v-model="trabajoSeleccionado.fechaFin" type="date"></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <p class="text-left font-weight-bold">Estado: </p>
+                  <v-combobox v-model="trabajoSeleccionado.estado" cleareable :items="['Planificado','En progreso','Finalizado']">
+                  </v-combobox>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-btn width="300" color="blue" class="text-none font-weight-regular" prepend-icon="mdi-check"
+                    text="GUARDAR" @click="editarTrabajoSeleccionado(trabajoSeleccionado)">
+                  </v-btn>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-btn width="300" color="orange" class="text-none font-weight-regular" prepend-icon="mdi-delete"
+                    text="ELIMINAR" @click="eliminarPlan(trabajoSeleccionado)">
+                  </v-btn>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-btn width="300" color="red" class="text-none font-weight-regular" prepend-icon="mdi-close"
+                    text="CANCELAR" @click="limpiarFormulario()">
+                  </v-btn>
+                </v-col>
+
+              </v-row>
+            </v-card-text>
+          </v-card>
+
+      </v-dialog>
+    </div>
+    
+    <v-data-table :headers="headers" :items="trabajos" item-key="ordenTrabajo" hide-default-footer>
+      <template v-slot:item="{ item }">
+        <tr>
+          <td>{{ item.ordenTrabajo }}</td>
+          <td v-for="header in headers.slice(1)" :key="header.key">
+            <v-sheet v-if="item.dias.includes(parseInt(header.key))" class="h-100 w-100" :color="item.color" 
+            @click="()=>{
+              verTrabajoSeleccionado(item);modoModal='Editar'; dialog=true
+              }"></v-sheet>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios';
-import { useDate } from 'vuetify'
-import { VCalendar } from 'vuetify/labs/VCalendar'
+import { useDate } from "vuetify";
+import axios from "axios";
+
 export default {
-components: {
-    VCalendar,
-},
-data () {
-    return{
-        dialog: false,
-        type: 'week',
-        weekday: [0, 1, 2, 3, 4, 5, 6],
-        value: new Date(),
-        events: [],
-        ordenesTrabajo: ['jamon','pollo'],
-        nuevoEvento: {},
-    }
-},
-methods: {
-    initializeNuevoEvento() {
-        return {
-            title: '',
-            start:new Date(''),
-            end:new Date(''),
-            color:'',
-        };
+  
+  data() {
+    return {
+      months: [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre",
+      ],
+
+      dialog:false,
+      modoModal:'',
+      ordenesT:['1','2','3'],
+
+      nuevoTrabajo:{},
+      trabajoSeleccionado:{
+        orden:'',
+        dias:[],
+        color:'',
+        estado:'',
+        fechaInicio:'',
+        fechaFin:''
+      },
+      
+      date: new Date(),
+      dateV: null,
+      weeks: useDate().getWeekArray(new Date()),
+
+      headers: [
+        { key: "ordenTrabajo", title: "Orden de Trabajo", align: "left" },
+      ], // Los dias se agregan segun el mes 'obtenerDiasMes()'
+      trabajos:[],
+    };
+  },
+  methods: {   
+    initializeNuevoTrabajo() {
+      return {
+        ordenTrabajo: '',
+        dias: [],
+        color:'',
+        fechaInicio:'',
+        fechaFin:'',
+        estado:''
+      };
     },
-    async fetchData() {
-        try {
-            const response = await axios.get("http://localhost:3000/api/plan-produccion/get/all");
-            console.log("Data:", response.data);
-            this.nuevoEvento = this.initializeNuevoEvento();  // Actualizar nuevoEvento después de obtener los ids
-            this.events=response.data.map((evento)=>{
-                return {
-                    ...evento,
-                    start: new Date(evento.start),
-                    end: new Date(evento.end)
-                }
-            })
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
+    //Prepara el array dias segun la fecha de inicio y fin 
+    diasCalendario(fechaInicio,fechaFin){
+      const date = this.dateV
+      const fechaI = date.getDate(new Date(`${fechaInicio}T00:00:00`))
+      const fechaF = date.getDate(new Date(`${fechaFin}T00:00:00`))
+
+      if(fechaF-fechaI<0) throw new Exception('La fecha de fin no puede ser menor que la fecha de inicio')
+
+      let diasC=[]
+      let j=0
+
+      for(let i=fechaI;i<=fechaF;i++){ diasC[j]=i;j++}
+
+      return diasC
     },
-    async obtenerOrdenTrabajo() {
-        try {
-            const response = await axios.get("http://localhost:3000/api/ordenTrabajo");
-            this.ordenesTrabajo = response.data.map(ordenTrabajo => ordenTrabajo.nombre);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
+    estados(e){
+      if(e=='Planificado'){
+        return 'blue'
+      }
+      if(e=='En progreso'){
+        return 'orange'
+      }
+      if(e=='Finalizado'){
+        return 'green'
+      }
     },
-    async agregarEvento() {
-        try {
-            const response = await axios.post(
-            "http://localhost:3000/api/plan-produccion/post",
-            this.nuevoEvento
-            );
-            console.log("Plan produccion agregado:", response.data);
-            // Actualiza la lista de proveedores
-            // Actualiza la lista de proveedores
-            await this.fetchData();
-            // Limpia los campos del formulario después de agregar
-            // this.headers.push({ key: 'actions', title: 'Acciones', sortable: false})
-            this.nuevoEvento = {
-                title: '',
-                start:'',
-                end:'',
-                color:'',
-            };
-            // Cierra el diálogo
-            this.dialog= false;
-        } catch (error) {
-            this.dialog= true;
-            console.error("Error al agregar proveedor:", error);
+    async agregarOrdenTrabajo() {
+      try {
+        //Agrega en el arreglo dias la lista de dias seleccionadas por inicio y fin de fecha
+        this.nuevoTrabajo.dias=this.diasCalendario(this.nuevoTrabajo.fechaInicio,this.nuevoTrabajo.fechaFin);
+        this.nuevoTrabajo.color=this.estados(this.nuevoTrabajo.estado)
+        const response = await axios.post('http://localhost:3000/api/plan-produccion/post',
+          this.nuevoTrabajo
+        );
+        this.nuevoTrabajo={
+          orden: '',
+          dias: [],
+          color:'',
+          fechaInicio:'',
+          fechaFin:'',
+          estado: ''
         }
+        console.log('Orden de compra agregada:', response.data);
+        this.trabajos.push(response.data);
+        await this.fetchData();
+        this.limpiarFormulario();
+      } catch (error) {
+        console.error('Error al agregar orden:', error);
+        this.modoModal = 'Agregar';
+      }
+    },
+    limpiarFormulario() {
+      this.nuevoTrabajo = this.initializeNuevoTrabajo();
+      this.trabajoSeleccionado = this.initializeNuevoTrabajo();
+      this.dialog = false;
+      this.modoModal = '';
+    },
+    async fetchData(){
+      try {
+        const response = await axios.get("http://localhost:3000/api/plan-produccion/get/all");
+        console.log("Data:", response.data);
+        this.trabajos = response.data
+
+        this.nuevoTrabajo = this.initializeNuevoTrabajo();  // Actualizar nuevaOrden después de obtener los ids
+        console.log(this.ids);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     },
 
-    // agregarEvento(){
-    //     this.events.push({
-    //         title: this.nuevoEvento.title,
-    //         start:new Date(this.nuevoEvento.start),
-    //         end:new Date(this.nuevoEvento.end),
-    //         color:this.nuevoEvento.color,
-    //     })
-    //     this.dialog= false;
-    //     this.limpiarFormulario();
-    // },
-    limpiarFormulario() {
-        this.nuevoEvento = this.initializeNuevoEvento();
-        if (this.$refs.formRef) {
-            this.$refs.formRef.reset(); // Resetea validación si es necesaria
-        }
-        this.dialog = false;
+    async verTrabajoSeleccionado(i){
+      const response = await axios.get(
+        "http://localhost:3000/api/plan-produccion/get/" + i._id
+      );
+      console.log("Orden selecionada:", response.data);
+      response.data.fechaInicio = this.formatearFecha(response.data.fechaInicio);
+      response.data.fechaFin = this.formatearFecha(response.data.fechaFin);
+
+      this.trabajoSeleccionado = response.data;
     },
-},
-mounted() {
+
+    async editarTrabajoSeleccionado(i){
+      try {
+        this.trabajoSeleccionado.dias=this.diasCalendario(this.trabajoSeleccionado.fechaInicio,this.trabajoSeleccionado.fechaFin);
+        this.trabajoSeleccionado.color=this.estados(this.trabajoSeleccionado.estado)
+        const response = await axios.put(
+          "http://localhost:3000/api/plan-produccion/" + i._id,
+          this.trabajoSeleccionado
+        );
+        console.log("Plan modificado:", response.data);
+        await this.fetchData();
+        this.dialog = false;
+        this.modoModal = "";
+      } catch (error) {
+        this.modoModal = "Editar";
+        this.dialogAgregarConfirmacion = true;
+        console.error("Error al editar Plan:", error);
+      }
+    },
+
+    async eliminarPlan(i){
+      try {
+        await axios.delete(`http://localhost:3000/api/plan-produccion/delete/${i._id}`);
+        alert('Plan eliminado');
+        this.dialog = false;
+        this.modoModal = '';
+        this.fetchData();
+      } catch (error) {
+        alert('No se pudo eliminar el plan');
+        console.error('Error al eliminar el plan:', error);
+      }
+    },
+
+    obtenerDiasMes() {
+      const date = this.dateV;
+
+      for (let i = 0; i < this.weeks.length; i++) {
+        for (let j = 0; j < this.weeks[i].length; j++) {
+          let nuevaFecha = {
+            key: "",
+            title: "",
+            align: "center",
+            sortable: false,
+          };
+
+          let mesActual = date.getMonth(new Date()) + 1;
+          let mesRecibido = date.getMonth(this.weeks[i][j]) + 1;
+
+          if (mesRecibido == mesActual) {
+            nuevaFecha.key = date.getDate(this.weeks[i][j]);
+            nuevaFecha.title = date.getDate(this.weeks[i][j]);
+
+            this.headers.push(nuevaFecha);
+          }
+        }
+      }
+    },
+
+    formatearFecha(fecha) {
+      const date = new Date(fecha);
+      const year = date.getUTCFullYear();
+      const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+      const day = String(date.getUTCDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
+    },
+    
+  },
+  mounted() {
+    this.dateV = useDate();
+    this.weeks = this.dateV.getWeekArray(new Date());
+    this.weekDays = this.dateV.getWeekdays();
+    this.obtenerDiasMes();
     this.fetchData();
-}
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+
+.v-table .v-table__wrapper table tbody tr td {
+  padding: 10px 0 10px 0;
+}
+
+.v-table .v-table__wrapper table tbody tr th {
+  padding: 0;
+}
 </style>
